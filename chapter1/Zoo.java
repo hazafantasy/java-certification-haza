db0c8250851ec6e3d89a28b7ec6f57481f99bc5f
hmojicacd <v-hmojica@ean.com>
2016-09-22 17:02:33 -0500

    Starting to study

---
+++ chapter1/Zoo.java
@@ -0,0 +1,10 @@
+public class Zoo {	
+	public static void main(String[] args) {
+        System.out.println("El Zoo chido");
+        if(args.length > 1) {
+            //Print first two parameters
+            System.out.println(args[0]);
+            System.out.println(args[1]);
+        }
+	}
+}
